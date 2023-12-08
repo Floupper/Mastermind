57febe1243969bafd3d2d15d38b48bc0eb586c49
h.hartz <henri.hartz@etu.unistra.fr>
2023-12-08 16:26:56 +0100

    début du jeu

--- Mastermind/Partie.java
+++ Mastermind/Partie.java
@@ -1,6 +1,71 @@
 package Mastermind;
 
 public class Partie {
-    protected static int nbPionsCombinaison = 4;
-    protected static int nbPions = 8;
+    private String joueur;
+    private Score _score;
+    private StrategyMode mode;
+    private Manche _manches;
+    protected static int nbManche = 3;//suppr les val de base
+    protected static int nbPionsCombinaison = 4;//suppr les val de base
+    protected static int maxTentatives = 10;//suppr les val de base
+    protected static int nbPions = 8;//suppr les val de base
+
+
+    public Partie()
+    {
+        this.nbManche = 3;
+        this.nbPionsCombinaison = 4;
+        this.maxTentatives = 10;
+        this.nbPions = 8;
+    }
+
+    public void setMode(StrategyMode mode)
+    {
+        this.mode = mode;
+    }
+
+    public void setNbManches(int nbManche)
+    {
+        this.nbManche = nbManche;
+    }
+
+    public void setNbPionCombinaison(int nbPionsCombinaison)
+    {
+        this.nbPionsCombinaison = nbPionsCombinaison;
+    }
+
+    public void setMaxTentatives(int maxTentatives)
+    {
+        this.maxTentatives = maxTentatives;
+    }
+
+    public void setNbPions(int nbPions)
+    {
+        this.nbPions = nbPions;
+    }
+
+    public StrategyMode getMode()
+    {
+        return this.mode;
+    }
+
+    public int getNbManches()
+    {
+        return this.nbManche;
+    }
+
+    public int getNbPionCombinaison()
+    {
+        return this.nbPionsCombinaison;
+    }
+
+    public int getMaxTentatives()
+    {
+        return this.maxTentatives;
+    }
+
+    public int getNbPions()
+    {
+        return this.nbPions;
+    }
 }
--- Mastermind/Score.java
+++ Mastermind/Score.java
@@ -1,5 +1,59 @@
 package Mastermind;
 
-public class Score {
+import java.util.ArrayList;
 
+public class Score {
+    private ArrayList<Integer> scoreManches;
+    private int scoreTotal;
+
+    public Score()
+    {
+        this.scoreTotal = 0;
+        this.scoreManches = new ArrayList<Integer>();
+    }
+
+    public int calculerScoreManche(LigneIndice ligneIndice, StrategyMode mode)
+    {
+        int score = 0;
+        ArrayList<Indice> indices = ligneIndice.getIndices();
+        for(Indice indice : indices)
+        {
+            switch(indice){
+                case MAL_PLACE:
+                    score += 1;
+                    break;
+                case BIEN_PLACE:
+                    score += 3;
+            }
+        }
+        //ajouter 4 si mode == classique
+        this.scoreManches.add(score);
+        return score;
+    }
+
+    public int calculerScoreTotal()
+    {
+        int score = 0;
+        for(int scoreManche : this.scoreManches)
+        {
+            score += scoreManche;
+        }
+        this.scoreTotal = score;
+        return score;
+    }
+
+    public int getScoreManche(Integer numManche)
+    {
+        return this.scoreManches.get(numManche);//! si numManche commence à 1
+    }
+
+    public ArrayList<Integer> getScoreManches()
+    {
+        return this.scoreManches;
+    }
+
+    public int getScoreTotal()
+    {
+        return this.scoreTotal;
+    }
 }
