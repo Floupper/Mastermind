2930de7d981945178e5e246e109c5225fad7dfe1
Mathieu Ackermann <mathieu.ackermann@etu.unistra.fr>
2024-01-13 17:01:57 +0100

    Ajout d'une mise en page 'basique' à terminer avec les retours à la ligne

--- Mastermind/EcranDemarrage.java
+++ Mastermind/EcranDemarrage.java
@@ -12,40 +12,66 @@ import javax.swing.JPanel;
 public class EcranDemarrage extends JFrame {
     private JButton btnJouer;
     private JButton btnCredits;
+    private JPanel panelBoutonsMenu;
     private JPanel menu;
+    private JLabel lblTitre;
+
     public EcranDemarrage() {
-        //définition de la page
+        // définition de la page
         super("ACKERMANN HARTZ - Mastermind");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1000, 600);
         setLocationRelativeTo(null);
         setResizable(false);
 
-        //boutton pour jouer
-        btnJouer = new JButton("Jouer");
+        // boutton pour jouer
+        btnJouer = new JButton();
+        btnJouer.setAlignmentX(CENTER_ALIGNMENT);
+        btnJouer.setPreferredSize(new Dimension(500, 30));
+        btnJouer.setText("Jouer");
         btnJouer.addActionListener(e -> {
-            new WindowSettings();
+            new EcranParametres();
             dispose();
         });
 
-        //boutton pour voir les crédits
-        btnCredits = new JButton("Crédits");
+        // boutton pour voir les crédits
+        btnCredits = new JButton();
+        btnCredits.setAlignmentX(CENTER_ALIGNMENT);
+        btnCredits.setAlignmentY(CENTER_ALIGNMENT);
+        btnCredits.setPreferredSize(new Dimension(500, 30));
+        btnCredits.setText("Crédits");
         btnCredits.addActionListener(e -> {
 
         });
 
-        //page du menu
+        // titre
+        lblTitre = new JLabel("Mastermind");
+        lblTitre.setAlignmentX(CENTER_ALIGNMENT);
+        lblTitre.setAlignmentY(CENTER_ALIGNMENT);
+        lblTitre.setFont(lblTitre.getFont().deriveFont(64.0f));
+        lblTitre.setPreferredSize(new Dimension(1000, 200));
+
+        JLabel presentation = new JLabel();
+        presentation.setAlignmentX(CENTER_ALIGNMENT);
+        presentation.setAlignmentY(CENTER_ALIGNMENT);
+        presentation.setText(
+                "Bienvenue sur le jeu du Mastermind ! \n Le but du jeu est de trouver la combinaison secrète de l'ordinateur. \n Pour cela, vous devez proposer une combinaison de pions de couleurs différentes. \n  L'ordinateur vous indiquera ensuite le nombre de pions bien placés et le nombre de pions mal placés. \n Vous avez 10 tentatives pour trouver la combinaison secrète. \n Bonne chance !");
+        presentation.setPreferredSize(new Dimension(1000, 200));
+
+        panelBoutonsMenu = new JPanel();
+        panelBoutonsMenu.setLayout(new BoxLayout(panelBoutonsMenu, BoxLayout.Y_AXIS));
+        panelBoutonsMenu.add(btnJouer);
+        panelBoutonsMenu.add(Box.createVerticalStrut(10));
+        panelBoutonsMenu.add(btnCredits);
+
+        // page du menu
         menu = new JPanel();
         menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
-        menu.add(Box.createVerticalGlue());
-        menu.add(Box.createHorizontalGlue());
+        menu.add(lblTitre);
+        menu.add(presentation);
+        menu.add(panelBoutonsMenu);
 
-        menu.add(btnJouer);
-        menu.add(btnCredits);
-        menu.add(Box.createHorizontalGlue());
-        menu.add(Box.createVerticalGlue());
-
-        //affichage de la page de base
+        // affichage de la page de base
         setContentPane(menu);
         setVisible(true);
     }
